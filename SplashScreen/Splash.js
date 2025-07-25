2e33aaa3dc041de9be279ab35a0c5e88f2cbb2b2
Sruthi Balasubramaniam <001b.sruthi@gmail.com>
2025-07-25 18:08:38 +0530

    changed header

--- SplashScreen/Splash.js
+++ SplashScreen/Splash.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
         // Redirect after the fade-out effect completes
         setTimeout(() => {
-            window.location.href = '../Home/Home.html';
+            window.location.href = '../ContentSruthi/Content.html';
         }, 1000); // Adjust this delay to match the transition duration
     }, 4000); // Initial delay before starting the fade-out
 });
